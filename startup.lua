6a91ee437447260da0955034fb087cee9f534c14
Sethas <sethas@ingame.name>
2025-06-28 13:37:49 +0300

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -9,7 +9,7 @@ local glass = 4
 local spotloader = 5
 local bucket = 4
 local floorY = 253
-local useGlassChest = false
+useGlassChest = true
 os.setComputerLabel("SethBot" .. os.getComputerID())
 os.sleep(2)
 position.x, position.y, position.z = gps.locate() --{x=0,y=0,z=0}
